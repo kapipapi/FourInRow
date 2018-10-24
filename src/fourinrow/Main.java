50c2fd20f7c805d83530bb7f918c00245ed7d295
Kacper Ledwosiński <kacper.led@gmail.com>
2018-10-24 20:36:34 +0200

    Initial commit

---
+++ src/fourinrow/Main.java
@@ -0,0 +1,22 @@
+package fourinrow;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] plansza = new int [7][6]; //jest 7 kolumn i 6 wierszy w planszy
+
+//          0 1 2 3 4 5 6
+//      0  [][][][][][][]
+//      1  [][][][][][][]
+//      2  [][][][][][][]
+//      3  [][][][][][][] :) // DATA ROZPOCZECIA: 01:45:00 17.08.2018r.
+//      4  [][][][][][][]
+//      5  [][][][][][][]
+
+
+    }
+
+    public
+
+}
